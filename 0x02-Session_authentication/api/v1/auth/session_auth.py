b8a962f29d6214bba7ae6293a706d9db545d4d2a
iamprincetj <tochukwunwanze5@gmail.com>
2023-11-08 21:32:55 +0100

    session_auth.py done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,15 +21,14 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns the user id of a User """
+        """Returns a User ID based on a Session ID"""
+
         if session_id is None or not isinstance(session_id, str):
             return None
-        
-        user_id = self.user_id_by_session_id.get(session_id)
 
-        return user_id
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """Returns a User instance based on a cookie value"""
@@ -42,3 +41,25 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Deletes de user session / logout"""
+
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
+
+        return True
